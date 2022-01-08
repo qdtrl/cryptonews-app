a557627eac9f38d561573b3e002cc50652f1159a
qdtrl <53430120+qdtrl@users.noreply.github.com>
2022-01-08 19:48:24 +0100

    Update cryptoApi.tsx

--- src/services/cryptoApi.tsx
+++ src/services/cryptoApi.tsx
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': '6c32d6e633mshe9a2a00403bb2d4p1cdb50jsne55244eddcc0'
+ 
 };
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
